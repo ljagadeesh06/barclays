3967da7f9b83f1c8b88839a47015967651803593
Jagadeesh L <34612644+ljagadeesh06@users.noreply.github.com>
2018-01-22 09:02:40 -0500

    Comments for understanding

--- src/main/java/com/barclays/designtest/trafficsignal/core/Constants.java
+++ src/main/java/com/barclays/designtest/trafficsignal/core/Constants.java
@@ -23,11 +23,16 @@ public interface Constants {
     String SOUTH = "S";
     String EAST  = "E";
     String WEST = "W";
+//    Time taken by the first car to move after green light - in seconds
     int GREEN_WAIT_TIME = 2;
+//    Max time for Green light - in seconds
     int GREEN_MAX_TIME = 3;
+//    Max time for Red light - in seconds
     int RED_MAX_TIME = 1;
     int SIGNAL_RESET = 0;
+//    Number of times a traffic update needs to be repeated
     int MAX_REPEATS = 21;
+//    Time delay between each traffic update - in milliseconds
     int THREAD_SLEEP_TIME = 1000;
 
 }
